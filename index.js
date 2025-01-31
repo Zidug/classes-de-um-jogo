bddfc876c14b76e1453d7349fc6c5c6e493dfa53
Zidug <gustavodurso@gmail.com>
2025-01-30 23:51:44 -0300

    Criação da logica
    foi montado a estrutura utilizando uma classe e um método para avaliar qual seria o personagem e o tipo de ataque

---
+++ index.js
@@ -0,0 +1,34 @@
+class nossoHeroi{
+    constructor(nomeHeroi, idadeHeroi, classeHeroi){
+        this.nomeHeroi = nomeHeroi
+        this.idadeHeroi = idadeHeroi
+        this.classeHeroi = classeHeroi
+    }
+
+    ataqueHeroi(){
+        let tipoAtaque = ""
+        switch (this.classeHeroi){
+            case "mago":
+            tipoAtaque = "usou magia"
+            break
+
+            case "guerreiro":
+            tipoAtaque = "usou espada"
+            break
+
+            case "monge":
+            tipoAtaque = "usou artes marciais"
+            break
+
+            case "ninja":
+            tipoAtaque = "usou shuriken"
+            break
+        }
+
+        console.log(`O ${this.classeHeroi} atacou usando ${tipoAtaque}`)
+    }
+}
+
+let heroi = new nossoHeroi("Zidug","28", "monge")
+
+heroi.ataqueHeroi()
